cb2fa8cd92d551c08fda95ad25a215353111bf2d
Salman <salman.naeem.112215@gmail.com>
2022-09-01 05:33:40 +0500

    removed extra import

--- lib/responsive_layout/responsiveLayout.dart
+++ lib/responsive_layout/responsiveLayout.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import './dimension.dart';
+import 'package:flutter/material.dart';
 
 class ResponsiveLayout extends StatelessWidget {
   final mobileLayout;
